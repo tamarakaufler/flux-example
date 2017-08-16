8f66f9782aa96b9610e64fed1838ea417573e5d4
Tamara Kaufler <tamara@weave.works>
2017-08-16 16:13:19 +0100

    Change 1

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -12,7 +12,7 @@ var rev = "devel"
 func main() {
 	var (
 		addr = flag.String("addr", ":80", "HTTP listen address")
-		msg  = flag.String("msg", "Hello, world", "Message to print")
+		msg  = flag.String("msg", "Hello, my new world", "Message to print")
 	)
 	flag.Parse()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
